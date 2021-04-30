a1dabf2c1e2ef777cd39c34c8a6d68d71f4ade67
Unknown <provincerice@gmail.com>
2021-04-29 22:28:09 -0600

    Added Scaffolding

---
+++ app/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :projects
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
+Rails.application.routes.draw do
+  root "projects#index"
+  resources :projects
+end
