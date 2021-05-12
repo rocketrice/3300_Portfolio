ec9e98b68e2703654cbca444a006a6afaa5c7811
Unknown <provincerice@gmail.com>
2021-05-12 11:24:03 -0600

    oop

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  rails g devise user
+  devise_for :users
   resources :projects
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
 Rails.application.routes.draw do
-  devise_for :users
   root "projects#index"
   resources :projects
 end
